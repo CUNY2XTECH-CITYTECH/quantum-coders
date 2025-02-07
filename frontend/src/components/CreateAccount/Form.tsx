934653868bd53b62eb030bc4fbfc6a5aab0eabe9
yuzhench2004 <yuzhench2004@gmail.com>
2025-02-07 21:11:29 +0000

    new feacture

--- frontend/src/components/CreateAccount/Form.tsx
+++ frontend/src/components/CreateAccount/Form.tsx
@@ -4,7 +4,7 @@ import { RiLockPasswordLine, RiEyeLine, RiEyeCloseLine } from "react-icons/ri";
 import { MdOutlineAccountCircle, MdOutlineDriveFileRenameOutline } from "react-icons/md";
 import { IoSend } from "react-icons/io5";
 
-//something
+//update
 export const FormSignUp = () => {
     //negate 
     //const navigate = useNavigate();
