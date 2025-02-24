9a9713275dde0aecea4e680e6cd5beeebc9935dd
Yu Chan <Yuzhench2004@gmail.com>
2025-02-24 13:53:48 -0500

    updating main page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -13,7 +13,7 @@ import Services from "./components/HomePage/Services";
 import About from "./components/HomePage/AboutUs";
 import Contact from "./components/HomePage/Contact";
 
-import './config/supertokensConfig'
+//import './config/supertokensConfig'
 /*
 //main initalize the superTokens, not need to import the superTokens in the login-form.tsx and RegisterForm.tsx
 import SuperTokens from "supertokens-auth-react";
