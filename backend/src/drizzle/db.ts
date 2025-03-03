6ea0254899a787a010cd55b09272da5af941ff37
Yu Chan <Yuzhench2004@gmail.com>
2025-03-02 23:55:49 -0500

    Prisma/Drizzle ORM schema

--- backend/src/drizzle/db.ts
+++ backend/src/drizzle/db.ts
@@ -6,3 +6,18 @@ dotenv.config();
 
 const connection = postgres(process.env.DATABASE_URL!); 
 export const db = drizzle(connection);
+//endpoin profile 
+import 'dotenv/config';
+//import { drizzle } from 'drizzle-orm/node-postgres';
+import { Client } from 'pg';
+
+// PostgreSQL Client
+const client = new Client({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+client.connect()
+  .then(() => console.log('✅ Connected to PostgreSQL'))
+  .catch((err) => console.error('❌ DB Connection Error:', err));
+
+export const db = drizzle(client);
